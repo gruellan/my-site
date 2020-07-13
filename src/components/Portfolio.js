1d7efa72ddb28e41161de1cc98880b6d46833d23
George Ruellan <ruellan.george@gmail.com>
2020-07-13 10:02:00 +0100

    Turn project panels into a map and add project modal

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,43 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ProjectPanel from './ProjectPanel'
 import { Container } from 'reactstrap'
-import Grid from '@material-ui/core/Grid';
+import Grid from '@material-ui/core/Grid'
+import Button from '@material-ui/core/Button'
+import DialogTitle from '@material-ui/core/DialogTitle'
+import Dialog from '@material-ui/core/Dialog'
 
-export default function Portfolio() {
+export default function Portfolio(props) {
+  const [currentPanel, setCurrentPanel] = useState("")
+  const [modalToggle, setModaltoggle] = React.useState(false)
+
+  const panels = [
+    {
+      id: 1,
+      title: 'Campus Bites',
+      desc: 'Co-founder and software developer of a food delivery service. Built a progressive web app where users could order food to be delivered to their university accommodation. Technologies used: VueJS & Firebase.',
+      image: 'campus_bites.png',
+      sourceCode: '#'
+    },
+    {
+      id: 2,
+      title: 'Smirkin - Digital Photobooths',
+      desc: 'Building and maintainng a photobooth app where customers at a venue can take selfies, apply filters and email it to themselves. Built using React Native.',
+      image: 'smirkin.jpg',
+      sourceCode: '#',
+      launch: 'https://www.smirkin.co.uk'
+    },
+  ]
+
+  const closeModal = () => {
+    setModaltoggle(prev => !prev)
+  }
+
   return (
     <Container>
+      <Dialog open={modalToggle} onClose={closeModal} >
+        <DialogTitle id="simple-dialog-title">HELLO</DialogTitle>
+      </Dialog>
       <Grid container
         spacing={2}
         style={{ marginTop: 50 }}
       >
-        <Grid item xs={12} md={6} lg={4}>
-          <ProjectPanel
-            title='Campus Bites Ltd'
-            desc='Co-founder and software developer of a food delivery service. Built a progressive web app 
-          where users could order food to be delivered to their university accommodation. Technologies 
-          used: VueJS Firebase.'
-            image='campus_bites.png'
-            sourceCode='#'
-          />
-        </Grid>
-        <Grid item xs={12} md={6} lg={4}>
-          <ProjectPanel
-            title='Smirkin - Digital Photobooths'
-            desc='Building and maintainng a photobooth app where customers
-          at a venue can take selfies, apply filters and email it to themselves. Built using React Native.'
-            image='smirkin.jpg'
-            launch='https://www.smirkin.co.uk'
-          />
-        </Grid>
-        <Grid item xs={12} md={6} lg={4}>
-          <ProjectPanel
-            title='Smirkin - Digital Photobooths'
-            desc='Building and maintainng a photobooth app where customers
-          at a venue can take selfies, apply filters and email it to themselves. Built using React Native.'
-            image='smirkin.jpg'
-            launch='https://www.smirkin.co.uk'
-          />
-        </Grid>
+        {panels.map((panel, i) => {
+          return (
+            <Grid item
+              xs={12} md={6} lg={4}
+              onClick={closeModal}
+            >
+              <ProjectPanel
+                title={panel.title}
+                desc={panel.desc}
+                image={panel.image}
+                sourceCode={panel.sourceCode}
+                launch={panel.launch}
+              />
+            </Grid>
+          )
+        })}
       </Grid>
     </Container >
   )
